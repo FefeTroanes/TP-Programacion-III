01b172bd1df4a2f464ab64175c7e617fcabffabe
FefeTroanes <ftroanes@gmail.com>
2024-05-12 17:20:00 -0300

    BFS implemented
    Signed-off-by: FefeTroanes <ftroanes@gmail.com>

--- tp-pathfinding/src/pathfinder/search/bfs.py
+++ tp-pathfinding/src/pathfinder/search/bfs.py
@@ -11,17 +11,45 @@ class BreadthFirstSearch:
 
         Args:
             grid (Grid): Grid of points
-            
+
         Returns:
             Solution: Solution found
         """
         # Initialize a node with the initial position
         node = Node("", grid.start, 0)
 
-        # Initialize the explored dictionary to be empty
-        explored = {} 
-        
-        # Add the node to the explored dictionary
+        # Initialize the explored dictionary with the initial state
+        explored = {}
         explored[node.state] = True
-        
+
+        # Return if the node contains a goal state
+        if node.state == grid.end:
+            return Solution(node, explored)
+
+        # Initialize the frontier (a queue) with the initial node
+        frontier = QueueFrontier()
+        frontier.add(node)
+
+        while True:
+            #  Fail if the frontier is empty
+            if frontier.is_empty():
+                return NoSolution(explored)
+
+            # Remove a node from the frontier
+            current_node = frontier.remove()
+
+            # BFS
+            # Check if the current node contains a goal state
+            if current_node.state == grid.end:
+                return Solution(current_node, explored)
+
+            successors = grid.get_neighbours(current_node.state).items()
+            for action, neighbour_state in successors:
+                if neighbour_state not in explored:
+                    # Create a new neighbour
+                    # new_neighbour = Node("", neighbour_state, sum(current_node.cost, grid.get_cost(neighbour_state)), current_node, action)
+
+                    neighbour_node = Node("", neighbour_state , current_node.cost + grid.get_cost(neighbour_state), parent=current_node, action=action)
+                    frontier.add(neighbour_node)
+                    explored[neighbour_state] = True
         return NoSolution(explored)
