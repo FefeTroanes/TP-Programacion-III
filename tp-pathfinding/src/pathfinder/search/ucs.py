536701e634217c67225b072cce8b71f318ef7ba6
FefeTroanes <ftroanes@gmail.com>
2024-05-13 12:14:03 -0300

    UCS implemented
    Signed-off-by: FefeTroanes <ftroanes@gmail.com>

--- tp-pathfinding/src/pathfinder/search/ucs.py
+++ tp-pathfinding/src/pathfinder/search/ucs.py
@@ -1,5 +1,5 @@
 from ..models.grid import Grid
-from ..models.frontier import StackFrontier
+from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
@@ -15,13 +15,37 @@ class UniformCostSearch:
         Returns:
             Solution: Solution found
         """
-        # Initialize a node with the initial position
+        # Inicializando el nodo con la posicion inicial
         node = Node("", grid.start, 0)
 
-        # Initialize the explored dictionary to be empty
-        explored = {} 
-        
-        # Add the node to the explored dictionary
-        explored[node.state] = True
-        
+        #Inicializando un diccionario para los explorados
+        explored = {}
+
+        #Añadiendo el estado a los explorados con el costo
+        explored[node.state] = node.cost
+
+        #Creando la frontera con la clase "Cola Prioridad"
+        #,añadiendo el nodo y el costo
+        frontera = PriorityQueueFrontier()
+        frontera.add(node,node.cost)
+
+        #Lo unico que cambia a bfs y dfs es que se le agrega el costo por lo tanto
+        #se usa cola por prioridad
+        while not frontera.is_empty():
+            nodo = frontera.pop()
+            #Test objetivo
+            if grid.end==nodo.state:
+                return Solution(nodo,explored)
+
+            for accion,estado in grid.get_neighbours(nodo.state).items():
+
+                #Creamos el costo para despues compararlo con el costo del nuevo estado
+                costo = nodo.cost + grid.get_cost(nodo.state)
+                if estado not in explored or costo < explored[estado]:
+                    nodo_actual = Node("",estado,costo,nodo,accion)
+
+                    #Agregamos a los exlorados el estado con su costo
+                    #y a la frontera el nodo y el costo
+                    explored[estado] = costo
+                    frontera.add(nodo_actual,costo)
         return NoSolution(explored)
